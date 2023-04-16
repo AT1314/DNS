c2eed025d5f5e4bf40dadb7e14a8e0b5e80935bd
蔡昂廷 <caiangting@caiangtingdeMacBook-Air.local>
2023-04-15 21:11:56 -0400

    grab and review

--- dns.py
+++ dns.py
@@ -1,14 +1,17 @@
 import socket, glob
 
+# Define port and ip address
 port = 53
-
 ip = '127.0.0.1'
 
+# Establish a UDP socket for communication (UDP message: 512 octets or less)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the server
 sock.bind((ip, port))
 
 def load_zones():
-
+    """Load zone files."""
     jsonzone = {}
     zonefiles = glob.glob('zones/*.zone')
 
@@ -23,7 +26,7 @@ def load_zones():
 zonedata = load_zones()
 
 def getflags(flags):
-
+    """Get different types of the flags based on the data."""
     byte1 = bytes(flags[:1])
     byte2 = bytes(flags[1:2])
 
@@ -31,29 +34,38 @@ def getflags(flags):
 
     # Byte1
 
+    # Query(0) or response(1)
     QR = '1'
 
+    # Type of query
     OPCODE = ''
     for bit in range(1, 5):
         OPCODE += str(ord(byte1)&(1<<bit))
 
+    # Authoritative Answer
     AA = '1'
 
+    # Trucation 
     TC = '0'
 
+    # Recursion desired?
     RD = '0'
 
     # Byte2
 
+    # Recursion Available?
     RA = '0'
 
+    # Reserved for future use
     Z = '000'
 
+    # Response code
     RCODE = '0000'
 
     return int(QR+OPCODE+AA+TC+RD, 2).to_bytes(1, byteorder='big')+int(RA+Z+RCODE, 2).to_bytes(1, byteorder='big')
 
 def getquestiondomain(data):
+    """Get the domain name as well as the question type."""
     state = 0
     expectedlength = 0
     domainstring = ''
@@ -86,57 +98,71 @@ def getquestiondomain(data):
 def getzone(domain):
     global zonedata
 
+    # "domain" is a list
     zone_name = '.'.join(domain)
     return zonedata[zone_name]
-
 
 def getrecs(data):
     domain, questiontype = getquestiondomain(data)
     qt = ''
     if questiontype == b'\x00\x01':
-        qt = 'a'
+        qt = 'a '
 
     zone = getzone(domain)
 
     return (zone[qt], qt, domain)
 
 def buildquestion(domainname, rectype):
+    """Build the question query."""
     qbytes = b''
 
+    # Build question
     for part in domainname:
         length = len(part)
         qbytes += bytes([length])
 
         for char in part:
             qbytes += ord(char).to_bytes(1, byteorder='big')
-             
+
+    # Build question type        
     if rectype == 'a':
         qbytes += (1).to_bytes(2, byteorder='big')
 
+    # Build question count
     qbytes += (1).to_bytes(2, byteorder='big')
 
     return qbytes
 
 def rectobytes(domainname, rectype, recttl, recval):
 
+    # DNS compression result
     rbytes = b'\xc0\x0c'
 
+    # TYPE
     if rectype == 'a':
         rbytes = rbytes + bytes([0]) + bytes([1])
 
+    # CLASS
     rbytes = rbytes + bytes([0]) + bytes([1])
 
+    # TTL
     rbytes += int(recttl).to_bytes(4, byteorder='big')
 
+    # For an "a" record
     if rectype == 'a':
+        # RDLENGTH
         rbytes = rbytes + bytes([0]) + bytes([4])
 
+        # RDATA
         for part in recval.split('.'):
             rbytes += bytes([int(part)])
 
     return rbytes
 
 def buildresponse(data):
+    """Build response."""
+    # The first two byte of the data represent the transaction ID
+    # It is used for identifying the sent-back response
     TransactionID = data[:2]
     TID = ''
     for byte in TransactionID:
@@ -145,11 +171,11 @@ def buildresponse(data):
     # Get the flags
     Flags = getflags(data[2:4])
 
-    # Question count
+    # Question count (For practice, every query contains one question)
     QDCOUNT = b'\x00\x01'
 
     # Answer count
-    ANCOUNT = len(getrecs(data[12:])[0]).to_bytes(2, byteorder='big ')
+    ANCOUNT = len(getrecs(data[12:])[0]).to_bytes(2, byteorder='big')
 
     # Nameserver count
     NSCOUNT = (0).to_bytes(2, byteorder='big')
@@ -165,14 +191,22 @@ def buildresponse(data):
     # Get answer for query
     records, rectype, domainname = getrecs(data[12:])
 
+    # Build the dns question
     dnsquestion = buildquestion(domainname, rectype)
 
+    # Fill the dnsbody
     for record in records:
         dnsbody += rectobytes(domainname, rectype, record["ttl"], record["value"])
 
-    return dnsheard+dnsquestion+dnsbody
+    return dnsheader+dnsquestion+dnsbody
 
+# Main loop of the program
 while 1:
+    # Receive message
     data, addr = sock.recvfrom(512)
+
+    # Build response
     r = buildresponse(data)
+
+    # Respond correspondingly
     sock.sendto(r, addr)
